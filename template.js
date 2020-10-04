40a5f0b2326be261379f3f8d786e03cb47cee7e7
k163791 <k163791@nu.edu.pk>
2020-10-04 11:27:33 +0500

    initial commit

---
+++ template.js
@@ -0,0 +1,18 @@
+export default () => {
+  return `<!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          >
+          <title>JWT Practice</title>
+        </head>
+        <body style="margin:0">
+          <div id="root"></div>
+          
+         
+        </body>
+      </html>`;
+};
